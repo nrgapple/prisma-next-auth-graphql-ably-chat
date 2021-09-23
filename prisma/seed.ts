73be2d4ee2709e47912b4acd590b21624a3d1656
Adam <champtoday@gmail.com>
2021-09-23 14:02:15 +0000

    🔧 Clean out code

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,74 +1,14 @@
-import { PrismaClient, ProfileStatus, ReferType, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const userData: Prisma.UserCreateInput[] = [
   {
     email: 'nick@test.com',
     name: 'nick',
-    profile: {
-      create: {
-        type: ReferType.referrer,
-        referrer: {
-          create: {
-            status: ProfileStatus.looking,
-            company: {
-              create: {
-                name: 'Facebook',
-              },
-            },
-          },
-        },
-        referent: {
-          create: {
-            status: ProfileStatus.disabled,
-            yearsExp: 0,
-          },
-        },
-      },
-    },
   },
   {
     email: 'ruddy@test.com',
     name: 'ruddy',
-    profile: {
-      create: {
-        type: ReferType.referent,
-        referrer: {
-          create: {
-            status: ProfileStatus.disabled,
-            company: undefined,
-          },
-        },
-        referent: {
-          create: {
-            status: ProfileStatus.looking,
-            yearsExp: 4,
-            experiences: {
-              create: [
-                {
-                  name: 'Cool Place to Work',
-                  title: 'Lead Dev',
-                  description: '- The man there.\n -Could not be better.',
-                },
-              ],
-            },
-            jobPostings: {
-              create: [
-                {
-                  name: 'Staff Dev',
-                  company: {
-                    connect: {
-                      id: 1,
-                    },
-                  },
-                  url: 'https://www.facebook.com/careers/v2/jobs/4006578832705963/?location=Washington%2C%20DC&utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic',
-                },
-              ],
-            },
-          },
-        },
-      },
-    },
   },
 ]
 
